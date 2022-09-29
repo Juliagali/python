f6c77963eca0e7849e8d5c9501537ab1531697c7
Juliagali <juliagalibarroso@gmail.com>
2022-09-29 16:05:36 -0300

    ler nome dos times, número de gols e mostrar o placar

---
+++ Lógica8.py
@@ -0,0 +1,8 @@
+time = (input("Insira o nome de um time de futebol: "))
+time2 = (input("Insira o nome de outro time de futebol: "))
+gols1 = (input("Digite o número de gols do primeiro time: "))
+gols2 = (input("Digite o número de gols do segundo time: "))
+
+
+
+print("\nO placar do jogo é: ", time, gols1," X ",gols2, time2)
