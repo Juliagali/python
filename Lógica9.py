5f048c82fcc8266826505e4cd30f59b988cc5617
Juliagali <juliagalibarroso@gmail.com>
2022-09-29 16:07:17 -0300

    imprimir empate ou qual foi o time vencedor

---
+++ Lógica9.py
@@ -0,0 +1,14 @@
+time = (input("Insira o nome de um time de futebol: "))
+time2 = (input("Insira o nome de outro time de futebol: "))
+gols1 = (input("Digite o número de gols do primeiro time: "))
+gols2 = (input("Digite o número de gols do segundo time: "))
+
+
+
+print("\nO placar do jogo é: ", time, gols1," X ",gols2, time2)
+if gols1 > gols2:
+    print("O time {} é o vencedor" .format(time))
+if gols1 < gols2:
+    print("O time {} é o vencedor" .format(time2))
+    else:
+         print("Empate")
