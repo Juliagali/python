6f2ad17cfa30be582785f6edbee2d87e21d73a23
Julia Gali <99364759+Juliagali@users.noreply.github.com>
2022-09-30 16:02:09 -0300

    ler e imprimir nomes dos times de futebol

--- Lógica06.py
+++ Lógica06.py
@@ -1,4 +1,4 @@
 time = (input("Insira o nome de um time de futebol: "))
 time2 = (input("Insira o nome de outro time de futebol: "))
 
-print("\nTimes de futebol inseridos: ", time, " e ", time2)
+print("\nNome dos times de futebol inseridos: ", time, " e ", time2)
